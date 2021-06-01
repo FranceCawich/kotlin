1444c2667b4974a3912135f53eaa012db3ca1276
FranceCawich <L17390321@chetumal.tecnm.mx>
2021-06-01 16:46:03 -0600

    borar

--- app/src/main/java/com/example/rfc/MainActivity.kt
+++ app/src/main/java/com/example/rfc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rfc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,20 @@ class MainActivity : AppCompatActivity() {
 
 
        val buttonBora = findViewById < Button >(R.id.buttonBora)
-        buttonBora.setOnClickListener(i:viewOnClickListener)
+       val editText1 = findViewById<EditText>(R.id.editTextTextPersonName)
+        val editText2 = findViewById<EditText>(R.id.editTextTextPersonName2)
+        val editText3 = findViewById<EditText>(R.id.editTextTextPersonName3)
+        val editText4 = findViewById<EditText>(R.id.editTextTextPersonName4)
+        val editText5 = findViewById<EditText>(R.id.editTextTextPersonName5)
+        val editText6 = findViewById<EditText>(R.id.editTextTextPersonName6)
+
+
+
+        buttonBora.setOnClickListener {     // Set the first EditText empty
+            editText1.setText("");
+
+
+        }
 
 
 
