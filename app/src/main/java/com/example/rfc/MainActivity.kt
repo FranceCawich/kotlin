272d905b3728a56f1a9a8f3713470dfeba6253b6
FranceCawich <L17390321@chetumal.tecnm.mx>
2021-06-03 16:23:18 -0600

    rfc parte2

--- app/src/main/java/com/example/rfc/MainActivity.kt
+++ app/src/main/java/com/example/rfc/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.rfc
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-       val buttonBora = findViewById < Button >(R.id.buttonBora)
-       val editText1 = findViewById<EditText>(R.id.editTextTextPersonName)
+       val buttonBora = findViewById <Button>(R.id.buttonBora)
+       val buttonRFc = findViewById<Button>(R.id.RFC)
+
+       val editText1 = findViewById<EditText>(R.id.one)
         val editText2 = findViewById<EditText>(R.id.editTextTextPersonName2)
+
         val editText3 = findViewById<EditText>(R.id.editTextTextPersonName3)
         val editText4 = findViewById<EditText>(R.id.editTextTextPersonName4)
         val editText5 = findViewById<EditText>(R.id.editTextTextPersonName5)
@@ -21,19 +29,25 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         buttonBora.setOnClickListener {     // Set the first EditText empty
-            editText1.setText("");
-            editText2.setText("");
-            editText3.setText("");
-            editText4.setText("");
-            editText5.setText("");
-            editText6.setText("");
+            editText1.setText("")
+            editText2.setText("")
+            editText3.setText("")
+            editText4.setText("")
+            editText5.setText("")
+            editText6.setText("")
+        }
+
+        buttonRFc.setOnClickListener {
+            val msg = editText1.text.toString().substring(0,3);
+            val text = findViewById<TextView>(R.id.textView99).apply {
+                text = msg
+            }
 
 
 
         }
-
-
 
     }
 }
