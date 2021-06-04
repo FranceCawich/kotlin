6d4f357f59b467a59ea52207eb2d5df7a5847b78
FranceCawich <L17390321@chetumal.tecnm.mx>
2021-06-03 23:38:44 -0600

    ajustes en RFC

--- app/src/main/java/com/example/rfc/MainActivity.kt
+++ app/src/main/java/com/example/rfc/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonBora = findViewById <Button>(R.id.buttonBora)
+        val buttonBora = findViewById<Button>(R.id.buttonBora)
         val buttonRFc = findViewById<Button>(R.id.RFC)
 
         val editText1 = findViewById<EditText>(R.id.one)
@@ -24,6 +24,13 @@ class MainActivity : AppCompatActivity() {
         val editText4 = findViewById<EditText>(R.id.editTextTextPersonName4)
         val editText5 = findViewById<EditText>(R.id.editTextTextPersonName5)
         val editText6 = findViewById<EditText>(R.id.editTextTextPersonName6)
+        val one = findViewById<TextView>(R.id.textView99)
+        val pp = findViewById<TextView>(R.id.textView6)
+        val nom = findViewById<TextView>(R.id.textViewNO)
+        val ano = findViewById<TextView>(R.id.textView7año)
+        val mes = findViewById<TextView>(R.id.textViewMEs)
+
+        val dia = findViewById<TextView>(R.id.textViewdia)
 
         buttonBora.setOnClickListener {     // Set the first EditText empty
             editText1.setText("")
@@ -32,46 +39,28 @@ class MainActivity : AppCompatActivity() {
             editText4.setText("")
             editText5.setText("")
             editText6.setText("")
-        }
-
-    buttonRFc.setOnClickListener {
-        val msg = editText3.text.toString().substring(0,2);
-        val msg2 = editText2.text.toString().substring(0,1);
-        val msg3 = editText1.text.toString().substring(0,1);
-        val msg4 = editText6.text.toString().substring(0,2);
-        val msg5 = editText5.text.toString().substring(0,2);
-        val msg6 = editText4.text.toString().substring(0,2);
 
 
-      val ooo = findViewById<TextView>(R.id.textView99).apply {
-            text = msg
-        }
-        val pp = findViewById<TextView>(R.id.textView6).apply {
-            text =msg2
         }
 
-        val nom = findViewById<TextView>(R.id.textViewNO).apply {
-            text = msg3
+        buttonRFc.setOnClickListener {
+            val msg = editText3.text.toString().substring(0, 2);
+            val msg2 = editText2.text.toString().substring(0, 1);
+            val msg3 = editText1.text.toString().substring(0, 1);
+            val msg4 = editText6.text.toString().substring(0, 2);
+            val msg5 = editText5.text.toString().substring(0, 2);
+            val msg6 = editText4.text.toString().substring(0, 2);
+
+            one.apply { text = msg }
+            pp.apply { text = msg2 }
+            nom.apply { text = msg3 }
+            ano.apply { text = msg4 }
+            mes.apply {  text = msg5 }
+            dia.apply { text = msg6 }
+
         }
 
-        val ano = findViewById<TextView>(R.id.textView7año).apply {
-            text = msg4
-        }
-        val mes = findViewById<TextView>(R.id.textViewMEs).apply {
 
-            text = msg5
-        }
-
-        val dia = findViewById<TextView>(R.id.textViewdia).apply {
-            text = msg6
-        }
     }
-    }
-
-
-
-
-
-
 
 }
